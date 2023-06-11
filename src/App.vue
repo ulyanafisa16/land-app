f5a3f6835b4cd85837ebd3ef6b90c69ed17206c1
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-06-11 23:50:57 +0700

    name

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h2> Nama: {{ nama }} </h2>
 </template>
+
+<script>
+
+export default {
+  name : "App",
+  data(){
+    return{
+      nama: "Ulya Nafis"
+    }
+  }
+};
+</script>
 
 <style lang="scss">
 #app {
