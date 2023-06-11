c11c3f7d7a58f65f9adc8eca8d6d09e979c88e5c
Ulya Nafisatussaniyah <ulyanafisa0@gmail.com>
2023-06-12 00:41:38 +0700

    conditional and looping

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <template>
+  <!--conditional and looping-->
+  <div v-if="products.length">
+  <ul>
+    <li v-for="item in products" :key="item.id">
+    {{ item.title }} - {{ item.price }}
+    </li>
+  </ul>
+</div>
+<div v-else>
+  <p>No data found</p>
+</div>
   <!--to way binding --> 
-  <input type="text" v-model="nama">
-  {{ nama }}
+  <!-- <input type="text" v-model="nama">
+  {{ nama }} -->
+
   <!-- <h2> Nama: {{ nama }} </h2>
   <button v-on:click="changeName">Change Name</button> -->
+
   <!-- data binding -->
   <!-- <a v-bind:href="url">Go to Ulya</a> -->
   <!-- <a :href="url">Go to Ulya</a> -->
 </template>
 
 <script>
-
 export default {
   name : "App",
   data(){
-    return{
+    return {
       // nama: "Ulya Nafis"
-      url : "https://github.com/ulyanafisa16?tab=repositories",
-      nama : "naff" ,
+      // url : "https://github.com/ulyanafisa16?tab=repositories",
+      // nama : "naff" ,
+      products:[
+        {id: 1, title:"Product 1", price:3000},
+        {id: 2, title:"Product 2", price:5000},
+        {id: 3, title:"Product 3", price:7000},
+        {id: 4, title:"Product 4", price:4000},
+        {id: 5, title:"Product 5", price:8000},
+        {id: 6, title:"Product 6", price:9000},
+        {id: 7, title:"Product 7", price:2000}
+      ]
     };
   },
   // methods:{
